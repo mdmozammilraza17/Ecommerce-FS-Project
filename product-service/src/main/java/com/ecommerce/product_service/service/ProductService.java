455c945fa921e7598d2ee700821fec185db16808
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2026-03-01 12:59:13 +0530

    Implemented RateLimiter in the product-service configuration

--- product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -1,8 +1,12 @@
 package com.ecommerce.product_service.service;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductService {
@@ -16,6 +20,7 @@ public class ProductService {
 
     @CircuitBreaker(name = "userServices", fallbackMethod = "fallbackUser")
     @Retry(name = "userServices")
+    @RateLimiter(name = "userServices", fallbackMethod = "fallbackRateLimitMsg")
     public String callUserService ()
     {
         String provideUrl = "http://USER-SERVICE/api/users/get";
@@ -24,5 +29,10 @@ public class ProductService {
 
     public String fallbackUser(Exception ex) {
         return "User service is currently unavailable";
+    }
+
+    public String fallbackRateLimitMsg (Throwable ex)
+    {
+        return "Too many request, Pls try again later...";
     }
 }
