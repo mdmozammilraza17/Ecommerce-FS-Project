7595ed4b0e965907bd5eb33345eca296c0541b2e
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2026-06-10 16:51:48 +0530

    Implemented Header at the top of Home page

--- ecommerce-frontend/src/components/Home.jsx
+++ ecommerce-frontend/src/components/Home.jsx
@@ -1,13 +1,39 @@
 import Footer from "./Footer";
 import "./Home.css";
 import "./Footer.css";
+import StdImage from '../assets/grocery-store.png';
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 export default function Home() {
   return (
     <>
-    <div className="home-container">
-      <h1>Home Page</h1>
+      <header className="home-container">
+        <div className="std-log-img">
+          <img src={StdImage} alt="Std General Store Image Logo" />
+        </div>
+
+        <div className="home-actions">
+          <nav>
+            <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Categories</li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="home-search">
+          <input type="text" placeholder="Search products..." />
+        </div>
+
+        <div className="home-login-signup">
+          <button>
+            <FaShoppingCart style={{ marginRight: "6px" }} />Cart (0)</button>
+          <button>
+            <FaUser style={{ marginRight: "6px" }} />Log In / Sign Up</button>
+        </div>
+      </header>
       <Footer/>
-</div>
     </>
   );
 }
