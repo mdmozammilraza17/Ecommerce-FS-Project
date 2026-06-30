8aa252edae529c03b703e52bde0a4b0bd5a55a8b
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2026-06-30 16:33:11 +0530

    Implemented get all banner by admin

--- banner-service/src/main/java/com/banner/controller/BannerController.java
+++ banner-service/src/main/java/com/banner/controller/BannerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,12 +25,22 @@ public class BannerController {
 
     private final BannerService bannerService;
 
+    // Create Banner
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping ("/create")
     public ResponseEntity<BannerResponseDTO> createBanner (@RequestParam ("image")MultipartFile image,
                                                            @Valid @ModelAttribute BannerRequestDTO bannerRequestDTO) throws BadRequestException {
         BannerResponseDTO response = bannerService.createBanner(image, bannerRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // Get All Banner
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping ("/banners")
+    public ResponseEntity<List<BannerResponseDTO>> getAllBanner ()
+    {
+        List<BannerResponseDTO> bannerResponseDTOS = bannerService.fetchAllBanner();
+        return ResponseEntity.ok(bannerResponseDTOS);
     }
 
     @GetMapping
--- banner-service/src/main/java/com/banner/service/BannerService.java
+++ banner-service/src/main/java/com/banner/service/BannerService.java
@@ -7,8 +7,12 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface BannerService {
     BannerResponseDTO createBanner(MultipartFile image,
                                           BannerRequestDTO bannerRequestDTO) throws BadRequestException;
+
+    List<BannerResponseDTO> fetchAllBanner();
+
 }
