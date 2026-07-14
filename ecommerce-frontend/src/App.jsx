749bf3019ff8d38576eb09894fc6849ea82023c3
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2026-07-14 23:43:11 +0530

    Integrating create page ui with backend

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Home from "./components/Home";
 import SignUp from "./components/Signup";
 import ProfileDropdown from "./components/ProfileDropdown";
@@ -6,11 +9,24 @@ import TopInfoBar from "./components/TopInfoBar";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/" element={<ProfileDropdown/>}/>
-      <Route path="/" element={<TopInfoBar/>}/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<ProfileDropdown />} />
+        <Route path="/" element={<TopInfoBar />} />
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+      />
+    </>
   );
 }
