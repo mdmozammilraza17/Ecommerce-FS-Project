1a9782b4ebde6032315425d32e7cb0bd4c95c39c
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2026-07-17 00:35:54 +0530

    Implement Verify OTP page in UI after while registring the user

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import SignUp from "./components/Signup";
 import ProfileDropdown from "./components/ProfileDropdown";
 import TopInfoBar from "./components/TopInfoBar";
+import VerifyOtp from "./components/VerifyOtp";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<ProfileDropdown />} />
         <Route path="/" element={<TopInfoBar />} />
+        <Route path="/verify-otp" element={<VerifyOtp/>}/>
       </Routes>
 
       <ToastContainer
